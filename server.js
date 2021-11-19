23deb9bc59520a8a1e8fd8d2dedd8e29dbf52afd
Lucy Dykhouse <l.dykhouse24@gmail.com>
2021-11-19 13:25:32 -0500

    Set up server

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const {bots, playerRecord} = require('./data')
-const {shuffleArray} = require('./utils')
+const express = require('express');
+const path = require('path');
+const app = express();
+const {bots, playerRecord} = require('./data');
+const {shuffleArray} = require('./utils');
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/styles", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.css"));
+  });
+
+app.get("/js", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.js"));
+});
 
 app.get('/api/robots', (req, res) => {
     try {
