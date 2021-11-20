8ccd56bcd8a82fd78f0b5bb78ec853ef955f5a0f
Lucy Dykhouse <l.dykhouse24@gmail.com>
2021-11-20 14:55:38 -0500

    Finished Part 4: Automated tests

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -1,4 +1,3 @@
-
 import { Builder, Capabilities, By } from "selenium-webdriver"
 
 require('chromedriver')
@@ -18,3 +17,21 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+
+test('Clicking the Draw button displays the div #choices', async () => {
+    await driver.findElement(By.id('draw')).click();
+    await driver.sleep(2000);
+    const choicesDiv = await driver.findElement(By.id('choices'));
+    const displayed = await choicesDiv.isDisplayed();
+    expect(displayed).toBe(true);
+});
+                                                ////*[text()="Hello, World"]
+test('Clicking the Add to Duo button displays the div #player-id', async () => {
+    await driver.findElement(By.id('draw')).click();
+    await driver.sleep(2000);
+    await driver.findElement(By.xpath("(//button[text()='Add to Duo'])[1]")).click();
+    await driver.sleep(2000);
+    const playerDiv = await driver.findElement(By.id('player-duo'));
+    const displayed = await playerDiv.isDisplayed();
+    expect(displayed).toBe(true);
+});
